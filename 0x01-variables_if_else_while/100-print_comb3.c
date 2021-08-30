ef92706defa36df0d381685dd9f949ad215c6b33
Briankipkemboirotich <briankemboirotich@gmail.com>
2021-08-30 04:12:02 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main -  print all possible different combinations of two digits separeted by ,
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	innt tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
