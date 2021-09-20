77770e5d0f0db93b844e025c1f1202549ff29353
Brian Rotich <briankemboirotich@gmail.com>
2021-09-20 08:16:58 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed 
+ *@argc: argument count
+ *@argv: array of pointer
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
