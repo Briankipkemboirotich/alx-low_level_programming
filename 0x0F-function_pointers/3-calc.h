98f853e7102325eab894301eb080ae9e479877d9
Brian Rotich <briankemboirotich@gmail.com>
2021-10-01 02:25:33 -0700

    Function pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -12,9 +12,8 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-	op_t;
-}
-	int op_add(int a, int b);
+} op_t;
+int op_add(int a, int b);
 
 int op_sub(int a, int b);
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -36,5 +36,4 @@ int main(int argc, char *argv[])
 		}
 		printf("%d\n", f(n1, n2));
 		return(0);
-		}
 }
